c428b8327e3bc71742cee0badc9254ae6eecaa77
Prakhar Ganeriwal <114392816+PRAKHARGAN@users.noreply.github.com>
2024-07-25 18:37:51 +0530

    For the removal of the manual input of auth code

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import streamlit as st
 from ui import main_app
+import os
+
+# Set page config
+st.set_page_config(page_title="Log Analysis Tool", layout="wide")
+
+# Set up environment variables
+os.environ['GOOGLE_CLIENT_ID'] = st.secrets["GOOGLE_CLIENT_ID"]
+os.environ['GOOGLE_CLIENT_SECRET'] = st.secrets["GOOGLE_CLIENT_SECRET"]
+os.environ['GOOGLE_REDIRECT_URI'] = st.secrets["GOOGLE_REDIRECT_URI"]
 
 def main():
     main_app()
