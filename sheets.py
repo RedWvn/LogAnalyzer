67bb5b273adb2f7d7dbe2bef80f5187a9aaca6d9
Prakhar Ganeriwal <114392816+PRAKHARGAN@users.noreply.github.com>
2024-07-25 18:39:51 +0530

    For the removal of the manual input of auth code

--- sheets.py
+++ sheets.py
@@ -1,37 +1,18 @@
-import os
-import pickle
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets.readonly']
+def build_sheets_service(credentials):
+    return build('sheets', 'v4', credentials=credentials)
 
-def authenticate_sheets():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    service = build('sheets', 'v4', credentials=creds)
-    return service
-
-def get_sheet_data(sheet_id, range_name):
-    service = authenticate_sheets()
+def get_sheet_data(service, sheet_id, range_name):
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=sheet_id, range=range_name).execute()
     values = result.get('values', [])
     return values
 
-def list_items(service, folder_id, mime_type):
-    query = f"'{folder_id}' in parents and mimeType='{mime_type}' and trashed=false"
-    results = service.files().list(q=query, spaces='drive', fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    return items
+def get_aircraft_info(service, sheet_id, sysid_thismav):
+    range_name = 'Aircraft List!A2:C'
+    values = get_sheet_data(service, sheet_id, range_name)
+    for row in values:
+        if row[0] == sysid_thismav:
+            return row[1], row[2]
+    return None, None
